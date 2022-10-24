e1c5701f2a17b3bf8f0c7d4870c284e3dd47ac9f
SalimmReza <salimnuaa@gmail.com>
2022-10-24 23:48:41 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="">
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
