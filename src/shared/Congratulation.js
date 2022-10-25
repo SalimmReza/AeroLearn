80f5ae4e00360195a28fe52ecfa87314479268cd
SalimmReza <salimnuaa@gmail.com>
2022-10-25 23:52:15 +0600

    congratulations page animation done

--- src/shared/Congratulation.js
+++ src/shared/Congratulation.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Congratulation.css'
 
 const Congratulation = () => {
     return (
         <div>
-            congo
+            <div className='di'>
+                <div className='mr-10'>
+                    <h1 className='text-white font-extrabold text-3xl'>Congratulations For Being With Us!</h1>
+                    <br />
+                    <h1 className='text-white font-extrabold text-2xl'  >Our Support is Fully With You!</h1>
+                </div>
+
+                <ul class="list-disc">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+
+                </ul>
+            </div>
         </div>
     );
 };
