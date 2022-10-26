e48551ac1031f694c10d5810263e9ea694168e21
SalimmReza <salimnuaa@gmail.com>
2022-10-26 16:24:31 +0600

    blog page done

--- src/Header/Pages/Blog.js
+++ src/Header/Pages/Blog.js
@@ -1,21 +1,48 @@
 import React, { useContext } from 'react';
-import ReactToPdf from 'react-to-pdf';
-const ref = React.createRef();
 
 
-
-const Blog = ({ }) => {
-
+const Blog = () => {
 
     return (
-        <div>
-            <ReactToPdf targetRef={ref} filename="div-blue.pdf">
-                {({ toPdf }) => (
-                    <button onClick={toPdf}>Generate pdf</button>
-                )}
-            </ReactToPdf>
-            <div ref={ref} >
-                <h1>jdfnjn</h1>
+        <div className='w-[70%] mx-auto mt-10 mb-[185px] '>
+            <div tabIndex={0} className="collapse group mt-3">
+                <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    what is cors?
+                </div>
+                <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.</p>
+                </div>
+            </div>
+
+            <div tabIndex={0} className="collapse group mt-3">
+                <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    Why are you using firebase? What other options do you have to implement authentication?
+                </div>
+                <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    <p>Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.</p>
+                    <br />
+                    <p>Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.</p>
+                </div>
+            </div>
+
+            <div tabIndex={0} className="collapse group mt-3">
+                <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    How does the private route work?
+                </div>
+                <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    <p>The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).</p>
+                </div>
+            </div>
+
+            <div tabIndex={0} className="collapse group mt-3">
+                <div className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    What is Node? How does Node work?
+                </div>
+                <div className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    <p>Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p>
+                    <br />
+                    <p>Working of Node.js: Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request.</p>
+                </div>
             </div>
         </div>
     );
